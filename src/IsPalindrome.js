03c78237c875516e9e29d7a3c7598929e9e86d8f
Pia-noder <charlotte.amiot@gmail.com>
2021-04-26 15:27:10 +0200

    isPalindrome résolut

---
+++ src/IsPalindrome.js
@@ -0,0 +1,46 @@
+//Code une fonction qui détermine si le mot est un palindrome
+
+//Comparer le mot d'origine avec son inverse; fonction intégrée dans 
+function comparedLetters(toCompare, reversed){
+	let x = 0;
+	//boucle qui ittére sur les lettres des 2 tableaux passés en argument est compare leur valeur 
+	//Valeur de x sert à s'assurer que l'on analyse toutes les lettres du mot 
+	//si lettre identique alors x += 1 sinon retourn false
+	  for(let i = 0; i < toCompare.length; i++){
+		if(toCompare[i]===reversed[i]){
+		  x++
+		}else{
+		  return false
+		};
+		//si on a bien ittéré sur toutes les lettres cela veux dire que le mot est un palindrome donc on retourne true
+		if(x === toCompare.length){
+		   return true;
+		 }  
+	  };
+	};
+	
+//Découper le mot de départ pour inverser les lettres
+function isPalindrome(word){
+	//Vérifier que l'on passe bien une string
+	if(typeof word === 'string'){
+
+    //Récupérer chaque lettre du mot individuellement 
+    let char = [];
+    let wordInTable = [];//garder le mot initial diviser dans un tableau
+      
+		for(let i = 0; i < word.length; i++){
+      char.push(word.charAt(i)); 
+          wordTable.push(word.charAt(i));
+    }
+
+    const reversed = char.reverse();
+
+    //appelle fonction pour comparer les lettres entre elles
+   return comparedLetters(wordInTable, reversed);
+	}
+    
+};
+
+console.log(isPalindrome('anne'))
+console.log(isPalindrome('anna'))
+isPalindrome('anne');
